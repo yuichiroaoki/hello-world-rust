8e0e2dc945dfe1eab77b09bf9fd9c2e8fca7722e
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-09 00:37:44 +0900

    config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,17 +2,29 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
+    // --snip--
+
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
