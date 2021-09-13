9a72fe56e47c55638f5803f5bc415882902ae7f2
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-13 20:57:27 +0900

    anyhow

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,22 +1,9 @@
-use structopt::StructOpt;
+use anyhow::{Context, Result};
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
-fn main() {
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
