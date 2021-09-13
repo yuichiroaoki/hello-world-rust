41fa3921f6454824406642cd7595f0343ae0566f
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-13 21:41:51 +0900

    log

---
+++ grrs/README.md
@@ -0,0 +1,5 @@
+```console
+env RUST_LOG=output_log=info cargo run --bin output-log
+```
+reference
+https://rust-cli.github.io/book/tutorial/output.html
--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::{Context, Result};
+use indicatif::ProgressBar;
+
 
 fn main() -> Result<()> {
+    let xs = vec![1, 2, 3];
+    println!("The list is: {:?}", xs);
+
     let path = "test.txt";
-    let content = std::fs::read_to_string(path)
-        .with_context(|| format!("could not read file `{}`", path))?;
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
     println!("file content: {}", content);
     Ok(())
 }
