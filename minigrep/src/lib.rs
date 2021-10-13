fc1a5172bd1397410033fcf501b86b2a2c454fec
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-10-13 21:48:23 +0900

    add comments

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -46,7 +46,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
     for line in contents.lines() {
@@ -58,7 +58,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn search_case_insensitive<'a>(
+fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,12 +4,19 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
+
+    // All values in Rust are stack allocated by default.
+    let some_value = "hello";
+
+    // arguments stored in heap
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
+    println!("{}, {}", some_value, args[0]);
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
