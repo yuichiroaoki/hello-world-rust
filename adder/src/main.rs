107cb48f00e21a2f4ba5ac806bdd734a04c7b30f
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-02-10 08:33:43 +0900

    get request

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,4 +1,24 @@
-fn main() {
-	println!("Run `cargo test`");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
 
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
+    s.clear(); // error!
+
+    // let hello = String::from("hello world");
+    let hello = "hello world";
+	let slice = &hello[..];
 }
--- cli-app/src/main.rs
+++ cli-app/src/main.rs
@@ -1,22 +1,68 @@
-use structopt::StructOpt;
-use serde_json::json;
+// use ansi_term::Style;
+// use clap::Parser;
+// use reqwest;
+// use serde_json::json;
+// use std::error::Error;
+// use ansi_term::Colour;
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// Output JSON instead of human readable messages
-    #[structopt(long = "json")]
-    json: bool,
+// #[derive(Parser, Debug)]
+// #[clap(author, version, about, long_about = None)]
+// struct Args {
+//     #[clap(short, long)]
+//     name: String,
+
+//     #[clap(short, long, default_value_t = 1)]
+//     count: u8,
+
+//     #[clap(long = "json")]
+//     json: bool,
+// }
+
+// fn main() {
+//     let args = Args::parse();
+
+//     for _ in 0..args.count {
+//         println!("Hello {}!", args.name)
+//     }
+//     println!("{} and this is not",
+//              Style::new().bold().paint("This is Bold"));
+//     println!("{}, {} and {}",
+//              Colour::Yellow.paint("This is colored"),
+//              Style::new().bold().paint("this is bold"),
+//              Colour::Yellow.bold().paint("this is bold and colored"));
+//     println!("This is {} in color, {} in color and {} in color",
+//              Colour::Red.paint("red"),
+//              Colour::Blue.paint("blue"),
+//              Colour::Green.paint("green"));
+//     if args.json {
+//         println!(
+//             "{}",
+//             json!({
+//                 "type": "message",
+//                 "content": "Hello world",
+//             })
+//         );
+//     } else {
+//         println!("Hello world");
+//     }
+// }
+
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
 }
 
-fn main() {
-    let args = Cli::from_args();
-    if args.json {
-        println!("{}", json!({
-            "type": "message",
-            "content": "Hello world",
-        }));
-    } else {
-        println!("Hello world");
-    }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
+    Ok(())
 }
